7a85ef4672e0d0e739c151a75102de2a7101a1d0
Yusuke Wada <yusuke@kamawada.com>
2021-10-01 16:54:01 +0900

    Add favicon

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,11 @@ import { router } from './router'
 
 const handleEvent = async (event: FetchEvent): Promise<Response> => {
   const requestUrl = new URL(event.request.url)
-  if (requestUrl.pathname === '/' || requestUrl.pathname.includes('static')) {
+  if (
+    requestUrl.pathname === '/' ||
+    requestUrl.pathname === '/favicon.ico' ||
+    requestUrl.pathname.includes('static')
+  ) {
     return await getAssetFromKV(event)
   } else {
     return await router.handle(event.request)
